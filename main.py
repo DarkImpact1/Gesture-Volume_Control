ad7ce4b3497cfb684fc6ffd1f75c4fa43f82bdf1
DarkImpact1 <dwivedimohit2001@gmail.com>
2024-09-26 20:08:25 +0530

    adding to module

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import cv2 as cv
 import mediapipe as mp
+import cv2 as cv
 import time
-import  tracking_module as tm
+import math
+import trackingModule as tm
+import numpy as np
+import pycaw
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 def displayFPS(image):
     global previous_time
@@ -10,19 +16,37 @@ def displayFPS(image):
     previous_time = current_time
     cv.putText(cam_image,f"{int(fps)} fps",(10,24),cv.FONT_HERSHEY_COMPLEX,1,(255,0,0),3)
 
-detector = tm.HandDetector()
-camera =  cv.VideoCapture(0)
-# variable to detect frame rate 
-previous_time, current_time = 0,0
+
+###############################################
+width_camera, height_camera = 640,480
+###########################################
+
+camera = cv.VideoCapture(0)
+camera.set(3,width_camera)
+camera.set(4,height_camera)
+
+
+# global variable for fps
+previous_time = 0
+
+# object to detect fist
+handDetector = tm.HandDetector(detectionConfidence=0.8)
+
+
+
 while True:
     success, cam_image = camera.read()
-    detected_hands = detector.findHand(image=cam_image,draw=True)
-    landmark_positions = detector.findPosition(image=cam_image)    
-    # print(landmark_positions)
-    
-    # Displaying FPS on screen 
-    displayFPS(cam_image)
+    cam_image = handDetector.findHand(cam_image,True)
+    landmark_positions = handDetector.findPosition(cam_image,draw=False)
+    cam_image,volume_bar,volume_percentage = handDetector.controlVolume(cam_image,landmark_positions)
 
+    if volume_bar:
+        # Display volume change through rectange
+        cv.rectangle(cam_image,(50,150),(85,400),(0,255,0),2)
+        cv.rectangle(cam_image,(50,int(volume_bar)),(85,400),(0,255,0),cv.FILLED)
+        cv.putText(cam_image,f"{int(volume_percentage)}%",(55,390),1,0.9,(0,0,0),2)
+
+    displayFPS(cam_image)
     cv.imshow("Main-camera",cam_image)
     if cv.waitKey(1) == ord('q'):
         break
